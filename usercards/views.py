c30c18fa5d2f44f2c2aba04fc85921d56e6b78d4
gri4on <igri4on@gmail.com>
2011-03-11 02:46:04 +0200

    ticket:3: Finished

--- usercards/views.py
+++ usercards/views.py
@@ -3,6 +3,7 @@ from django.forms.models import model_to_dict
 from test_for_coffe_cups.usercards.models import UserCard, MiddlewareData
 from test_for_coffe_cups.usercards.forms import CardForm
 from django.core.context_processors import csrf
+from django.template import RequestContext
 
 
 def contact(request):
@@ -10,7 +11,7 @@ def contact(request):
     Handler for main page of test
     """
     c = {"card": UserCard.objects.all()[0]}
-    return render_to_response("contacts.html", c)
+    return render_to_response("contacts.html", RequestContext(request, c))
 
 
 def edit_card(request):
@@ -23,7 +24,7 @@ def edit_card(request):
             form.save()
     c = {"form": CardForm(initial=model_to_dict(UserCard.objects.all()[0]))}
     c.update(csrf(request))
-    return render_to_response("edit_card.html", c)
+    return render_to_response("edit_card.html", RequestContext(request, c))
 
 
 def report_middleware(request):
@@ -31,4 +32,11 @@ def report_middleware(request):
     Show all stored requests
     """
     c = {"middleware_list": MiddlewareData.objects.all()}
-    return render_to_response("middleware_report.html", c)
+    return render_to_response("middleware_report.html", RequestContext(request, c))
+
+
+def context_processor(request):
+    """
+    Show settings from settings file, by context processor
+    """
+    return render_to_response("settings.html", RequestContext(request, {}))
