387cde1d773eab355f5f4d43bd0b13a739876f04
gri4on <igri4on@gmail.com>
2011-03-15 01:38:15 +0200

    t5: Cleanup code

--- usercards/tests.py
+++ usercards/tests.py
@@ -77,20 +77,18 @@ class TestMiddleware(TestCase):
 
 class TestEditUserCard(TestCase):
     def setUp(self):
-        # Do we need to create testUser or we can edit page with admin user ???
-         
         # create UserCard model with all fields
         client = Client()
         self.client = client
-        
-        # Test authentication 
-        #  1. Test http redirect 
+
+        # Test authentication
+        #  1. Test http redirect
         #     (if user is not authenticated - redirect to login page)
-        #  2. Test http auth 
+        #  2. Test http auth
         #     (post auth data and check for redirect to 'edit page')
         #  3. Edit data on page (post )
         response_auth = client.get('/edit_usercard/')
-        
+
         # redirect to auth page
         self.failUnlessEqual(response_auth.status_code, 302)
         response_auth = self.client.post('/accounts/login/',\
@@ -99,7 +97,7 @@ class TestEditUserCard(TestCase):
         self.failUnlessEqual(response_auth.status_code, 302)
         response_auth = client.get("/edit_usercard/")
         self.failUnlessEqual(response_auth.status_code, 200)
-        
+
         response = client.post('/edit_usercard/',\
                                  {'name'           : 'TestName',
                                   'last_name'      : 'TestLastName',
